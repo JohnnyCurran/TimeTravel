2633d04525779a643c15dca7565631811ee1e898
John Curran <john@talentwall.io>
2022-12-06 13:40:40 -0600

    Get rid of utils

--- lib/telemetry_handler.ex
+++ lib/telemetry_handler.ex
@@ -8,13 +8,9 @@ defmodule TimeTravel.TelemetryHandler do
 
     {:ok, clean_assigns} =
       metadata.socket.assigns
-      |> Utils.safe_assigns()
+      |> safe_assigns()
       |> Jason.encode()
 
-    # IO.inspect metadata.socket, label: "SOCK!"
-
-    # AllegroWeb.Endpoint.broadcast("lvdbg", "lv_event", %{payload: clean_assigns})
-    #
     # Positive: no negative number keys
     # monotonic: always increasing so the list can be sorted
     time = System.unique_integer([:positive, :monotonic])
@@ -33,5 +29,69 @@ defmodule TimeTravel.TelemetryHandler do
       time: time,
       name: event_name
     })
+  end
+
+  # Make assigns JSON serializable
+  def safe_assigns(assigns) do
+    Enum.reduce(assigns, %{}, fn
+      {_key, %Ecto.Association.NotLoaded{}}, acc ->
+        acc
+
+      {key, %DateTime{} = v}, acc ->
+        Map.put(acc, key, DateTime.to_iso8601(v))
+
+      {key, %Date{} = v}, acc ->
+        Map.put(acc, key, Date.to_iso8601(v))
+
+      {key, value}, acc when is_struct(value) ->
+        clean_struct =
+          value
+          |> Map.delete(:__meta__)
+          |> Map.from_struct()
+
+        Map.put(acc, key, safe_assigns(clean_struct))
+
+      {key, value}, acc when is_function(value) ->
+        Map.put(acc, key, inspect(value))
+
+      {key, value}, acc when is_map(value) ->
+        Map.put(acc, key, safe_assigns(value))
+
+      {key, value}, acc when is_list(value) ->
+        Map.put(acc, key, safe_assigns(value))
+
+      {key, value}, acc when is_tuple(value) ->
+        Map.put(acc, key, safe_assigns(Tuple.to_list(value)))
+
+      {_key, value}, acc when is_pid(value) ->
+        acc
+
+      {key, value}, acc ->
+        Map.put(acc, key, value)
+
+      %DateTime{} = v, _acc ->
+        DateTime.to_iso8601(v)
+
+      %Date{} = v, _acc ->
+        Date.to_iso8601(v)
+
+      v, _acc when is_struct(v) ->
+        v |> Map.delete(:__meta__) |> Map.from_struct() |> safe_assigns()
+
+      v, _acc when is_function(v) ->
+        inspect(v)
+
+      v, _acc when is_map(v) ->
+        safe_assigns(v)
+
+      v, _acc when is_tuple(v) ->
+        v |> Tuple.to_list() |> safe_assigns()
+
+      v, acc when is_pid(v) ->
+        acc
+
+      v, _acc ->
+        v
+    end)
   end
 end
