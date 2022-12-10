c4cafc40e691168af6ae058640e5eea3b7db6981
John Curran <john@talentwall.io>
2022-12-10 11:35:05 -0600

    handle_info callbacks are taken care of in before_compile module hook

--- lib/time_travel.ex
+++ lib/time_travel.ex
@@ -2,17 +2,25 @@ defmodule TimeTravel do
   @moduledoc """
   Documentation for `TimeTravel`.
   """
+  defmacro __using__(_) do
+    quote do
+      @before_compile TimeTravel
+    end
+  end
 
-  @doc """
-  Hello world.
+  defmacro __before_compile__(_env) do
+    quote do
+      def handle_info({:time_travel, _socket_id, nil}, socket) do
+        {:noreply, socket}
+      end
 
-  ## Examples
+      def handle_info({:time_travel, socket_id, assigns}, %{id: id} = socket) when id == socket_id do
+        {:noreply, assign(socket, assigns)}
+      end
 
-      iex> TimeTravel.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      def handle_info({:time_travel, _socket_id, _assigns}, params, socket) do
+        {:noreply, socket}
+      end
+    end
   end
 end
