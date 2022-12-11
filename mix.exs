23e5beca31b7ae8af3ea1ea5b3e233ec03928aa2
John Curran <john@talentwall.io>
2022-12-11 16:45:21 -0600

    Bump vsn number

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TimeTravel.MixProject do
   def project do
     [
       app: :time_travel,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11",
       description: description(),
       package: package(),
