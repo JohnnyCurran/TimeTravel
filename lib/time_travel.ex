5af5629fcfa071bc09026a2b8eb79b07c3b274cb
John Curran <john@talentwall.io>
2022-12-17 15:46:40 -0600

    use tuple to invoke

--- lib/time_travel.ex
+++ lib/time_travel.ex
@@ -2,25 +2,13 @@ defmodule TimeTravel do
   @moduledoc """
   Documentation for `TimeTravel`.
   """
-  def endpoint do
-    case Application.fetch_env(:time_travel, :endpoint) do
-      :error ->
-        raise """
-        Unable to retrieve :endpoint from config. Make sure you have specified "config :time_travel, :endpoint, YourAppWeb.Endpoint" inside of config/config.exs
-        """
-
-      {:ok, endpoint} ->
-        endpoint
-    end
-  end
-
   defmacro __using__(_) do
     quote do
-      @before_compile TimeTravel
+      @before_compile {TimeTravel, :live_view}
     end
   end
 
-  defmacro __before_compile__(_env) do
+  def live_view(__env__) do
     quote do
       def handle_cast({:time_travel, _socket_id, nil}, socket) do
         {:noreply, socket}
