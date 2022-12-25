0a3cd8a89dc4e7db434b0004033138818350e0e5
John Curran <john@talentwall.io>
2022-12-25 11:35:18 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TimeTravel.MixProject do
   def project do
     [
       app: :time_travel,
-      version: "0.2.2",
+      version: "0.3.0",
       elixir: "~> 1.11",
       description: description(),
       package: package(),
